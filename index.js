ec90dda4a93ef98c00d2bb9fa1095985f73c7549
KVCobbs <kenyacobbs9001@gmail.com>
2021-11-01 13:48:04 +0000

    finished first two

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 //1
-function greatestOfTwo(){
-  
+function greatestOfTwo(x, y){
+  if(x > y) {
+      console.log(x);
+  } else
+  console.log(y)
 }
 
 //2
-function greatestOfThree(){
-  
+function greatestOfThree(x, y, z){
+    if(x > y && x > z) {
+        console.log(x)
+    } else if (y > x && y > z) {
+        console.log(y)
+    } else if (z > x && z > y) {
+        console.log(z)
+    }
 }
 
 //3
@@ -18,3 +27,6 @@ function greatestOfFour(){
 function leastOfFour(){
   
 }
+
+//greatestOfTwo(5, 8)
+//greatestOfThree(20, 16, 98)
